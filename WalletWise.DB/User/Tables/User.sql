3b73e802d8185f11f6c77a275f90ae91fdf0c8c9
bertimatteo93@gmail.com <G@llier@0>
2024-08-08 19:59:04 +0200

    Implementa struttura database User

---
+++ WalletWise.DB/User/Tables/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [User].[User]
+(
+	[Id]           BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Username]     NVARCHAR(255) NOT NULL,
+	[Email]        NVARCHAR(255) NOT NULL,
+	[PasswordSalt] NVARCHAR(255) NOT NULL,
+	[Password]     NVARCHAR(255) NOT NULL
+)
+
+GO
+
+CREATE INDEX [IX_Username_Password] ON [User].[User] ([Username], [Password])
---
+++ WalletWise.Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace WalletWise.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordSalt { get; set; }
+        public string Password { get; set; }
+    }
+}
